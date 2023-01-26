1579cacec1a1790e96088a49db9355a3168cbdae
Natalie Chin <natalie.h.chin@gmail.com>
2023-01-26 14:18:20 -0800

    finished hw

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Natalie Chin</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is so cool!
         </p>
+        <p>this is another paragraph element</p>
+        <ul>
+          <li>
+            A
+          </li>
+          <li>
+            B
+          </li>
+          <li>
+            C
+          </li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
